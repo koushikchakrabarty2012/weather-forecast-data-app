a221f045e8e2f7d1beb1ee353aec6114ab47725d
koushikchakrabarty2012 <57660006+koushikchakrabarty2012@users.noreply.github.com>
2024-01-16 19:01:26 +0530

    Final Commit

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "0cb40cd157e6db21578293599dd2e848"
+
+
+def get_data(place, forcast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forcast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forcast_days=3, kind="Sky"))
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place :")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of Forecasted Days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-
 st.subheader(f"{option} for the next {days} days in {place}")
+if place:
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
+
+        match option:
+            case "Temperature":
+                temperatures = [i["main"]["temp"] / 10 for i in filtered_data]
+                type(temperatures)
+                dates = [d["dt_txt"] for d in filtered_data]
+                # Create a Temperature plot
+                figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+                st.plotly_chart(figure)
+            case "Sky":
+                sky_conditions = [i["weather"][0]["main"] for i in filtered_data]
+                print(sky_conditions)
+                images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                          "Rain": "images/rain.png", "Snow": "images/snow.png"}
+                image_paths = [images[i] for i in sky_conditions]
+                st.image(image_paths, width=115)
+    except KeyError:
+        st.write("That Place does not exist")
